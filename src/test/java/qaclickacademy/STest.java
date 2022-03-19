e0fd5c08ada8f98058975c91455687689c2fb630
shwetha <shwtha03@gmail.com>
2022-03-19 17:05:38 +0800

    first commit

---
+++ src/test/java/qaclickacademy/STest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class STest {
+
+ @Test
+public void browser()
+{
+	System.out.println("yeah");
+}
+
+	@Test
+public void systemlol()
+{
+	System.out.println("this is working now");
+}
+}
+
